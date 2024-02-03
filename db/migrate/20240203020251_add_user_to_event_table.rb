4a3af6cd7763cde95a79b0165c1039b543c7055f
angel23v <angelvazquezvirgen@gmail.com>
2024-02-02 20:19:36 -0600

    Filter view on events
    When you create a public event, all the users will be able to see it but if you create a private event you just gonna see it

---
+++ db/migrate/20240203020251_add_user_to_event_table.rb
@@ -0,0 +1,6 @@
+class AddUserToEventTable < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :events, :user, foreign_key: true
+    add_column :events, :privado, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_152000) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_020251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_152000) do
     t.float "costo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.boolean "privado"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_152000) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
